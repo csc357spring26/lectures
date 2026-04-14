09de7004fcada116ca141f3b898e7b48c6f22b25
Christopher E. Siu <cesiu@calpoly.edu>
2026-04-14 13:29:05 -0700

    Added examples of arrays as pointers.

--- 04.14-arrays/swap.c
+++ 04.14-arrays/swap.c
@@ -4,5 +4,18 @@ void swap(int *x, int *y) {
     *y = temp;
 }
 
+/* NOTE: Arguments are pass-by-value, and the value of an array is the address
+ *       of its zeroth element, which "decays" into a pointer. Note that the
+ *       length must be passed separately if needed. */
 void reverse(int arr[], int len) {
+    int i;
+
+    for (i = 0; i < len / 2; i++) {
+        swap(&arr[i], &arr[len - 1 - i]);
+
+        /* NOTE: Since arrays and pointers are both addresses; the compiler
+         *       lets us use array syntax on what is really a pointer. The
+         *       above is equivalent to:
+         * swap(arr + i, arr + (len - 1 - i)); */
+    }
 }
--- 04.14-arrays/test.c
+++ 04.14-arrays/test.c
@@ -1,16 +1,64 @@
 #include <stdio.h>
 #include "swap.h"
 
+/* NOTE: Pointers are variables in their own right, and they can be collected
+ *       into arrays just like ordinary variables. An array of pointers thus
+ *       decays into a double pointer when passed as argument. */
+void f(int **mat) {
+    mat[1][0] = 5;
+}
+
+/* NOTE: Multidimensional arrays are stored in "row-major order": a 2D array is
+ *       an array of rows, and the compiler must know the length of each row in
+ *       order to index the array. This only works on n x 2 arrays...*/
+void g(int mat[][2]) {
+    mat[1][0] = 6;
+}
+
+/* NOTE: ...since we the programmers know that this is not actually an ordinary
+ *       pointer, but instead the beginning of a 2D array, we can do the
+ *       indexing math for the compiler in order to accept any n x m array. */
+void h(int *mat, int width) {
+    *(mat + (1 * width) + 0) = 7;
+}
 
 int main(void) {
     int arr[] = {1, 2, 3, 4};
+    int row0[] = {1, 2}, row1[] = {3, 4};
+    int *mat1[2];
+    int mat2[][2] = {{1, 2}, {3, 4}};
+
+    mat1[0] = row0;
+    mat1[1] = row1;
 
     reverse(arr, 4);
+
     printf("arr: %p\n", (void *)arr);
     printf(" |- %p: %d\n", (void *)&arr[0], arr[0]);
     printf(" |- %p: %d\n", (void *)&arr[1], arr[1]);
     printf(" |- %p: %d\n", (void *)&arr[2], arr[2]);
     printf(" +- %p: %d\n", (void *)&arr[3], arr[3]);
+
+    f(mat1);
+
+    printf("mat1: %p\n", (void *)mat1);
+    printf(" |- %p: %p\n", (void *)&mat1[0], (void *)mat1[0]);
+    printf(" |   |- %p: %d\n", (void *)&mat1[0][0], mat1[0][0]);
+    printf(" |   +- %p: %d\n", (void *)&mat1[0][1], mat1[0][1]);
+    printf(" +- %p: %p\n", (void *)&mat1[1], (void *)mat1[1]);
+    printf("     |- %p: %d\n", (void *)&mat1[1][0], mat1[1][0]);
+    printf("     +- %p: %d\n", (void *)&mat1[1][1], mat1[1][1]);
+
+    g(mat2);
+    h((int *)mat2, 2);
+
+    printf("mat2: %p\n", (void *)mat2);
+    printf(" |- %p: %p\n", (void *)&mat2[0], (void *)mat2[0]);
+    printf(" |   |- %p: %d\n", (void *)&mat2[0][0], mat2[0][0]);
+    printf(" |   +- %p: %d\n", (void *)&mat2[0][1], mat2[0][1]);
+    printf(" +- %p: %p\n", (void *)&mat2[1], (void *)mat2[1]);
+    printf("     |- %p: %d\n", (void *)&mat2[1][0], mat2[1][0]);
+    printf("     +- %p: %d\n", (void *)&mat2[1][1], mat2[1][1]);
 
     return 0;
 }
