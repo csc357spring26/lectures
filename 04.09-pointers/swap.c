d59ae8c2365f4723a17a25f758e1f4ce95a460bf
Christopher E. Siu <cesiu@calpoly.edu>
2026-04-14 07:55:12 -0700

    Added examples of pointers.

--- 04.09-pointers/swap.c
+++ 04.09-pointers/swap.c
@@ -1,5 +1,13 @@
-void swap(int x, int y) {
-    int temp = x;
-    x = y;
-    y = temp;
+#include <stdio.h>
+
+/* NOTE: In C, arguments are always pass-by-value, but the value of a pointer
+ *       is a reference. By passing pointers, we can emulate pass-by-reference
+ *       behavior whenever we want. */
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+
+    printf("x (%p): %p\n", (void *)&x, (void *)x);
+    printf("y (%p): %p\n", (void *)&y, (void *)y);
 }
--- 04.09-pointers/test.c
+++ 04.09-pointers/test.c
@@ -4,10 +4,18 @@
 int main(void) {
     int z = 1, w = 2;
 
-    swap(z, w);
+    /* NOTE: Essentially, by telling a function *where* a value can be found
+     *       rather than *what* a value is, we can give any function access to
+     *       any other function's local variables! */
+    swap(&z, &w);
 
-    printf("z: %d\n", z);
-    printf("w: %d\n", w);
+    printf("z (%p): %d\n", (void *)&z, z);
+    printf("w (%p): %d\n", (void *)&w, w);
+
+    /* NOTE: A NULL pointer is guaranteed to be an invalid address, and so
+     *       dereferencing a NULL pointer will result in a segfault. A segfault
+     *       is never ideal, but consistent failure is better than randomness.
+     * swap(NULL, NULL); */
 
     return 0;
 }
