9402d30df6ca83a06f0a831725863f0cd78efa06
cesiu <cesiu@calpoly.edu>
2026-04-21 13:59:11 -0700

    Added example of structures.

--- 04.21-structures/list.c
+++ 04.21-structures/list.c
@@ -3,11 +3,44 @@
 
 /* lstcreate: Creates an empty linked list. */
 List *lstcreate(void) {
-    return NULL;
+    List *lst = (List *)malloc(sizeof(List));
+
+    /* NOTE: The value of a structure is the whole structure, so, for efficiency,
+     *       we typically pass around pointers to structures instead. This means
+     *       the structure will have to be dynamically allocated, so as to
+     *       avoid returning the address of a local variable. */
+    (*lst).head = NULL;
+
+    /* NOTE: This also means that we have to dereference the pointer before we
+     *       can access its members. Note that the "." ordinarily has precedence
+     *       over the "*". Alternatively, the "->" operator will dereference
+     *       the pointer before accessing the member. */
+    lst->size = 0;
+
+    return lst;
 }
 
 /* lstdestroy: Destroys an existing linked list. */
 void lstdestroy(List *lst) {
+    Node *node = lst->head;
+    
+    /* NOTE: Once we free memory, its contents are effectively random -- they
+     *       can be reused for future allocations. We might get lucky, and the
+     *       data we need might still happen to be there, but in general we
+     *       cannot access members after freeing their struct. */
+
+    while (node != NULL) {
+        Node *tmp = node->next;
+        free(node);
+        node = tmp;
+    }
+
+    /* NOTE: The compiler does not know what a "linked list" is; it has no way
+     *       of knowing what a List's memory represents, that a List is the
+     *       only way to access its Nodes, or that being done with a List
+     *       implies being done with its Nodes. */
+
+    free(lst);
 }
 
 /* lstget: Gets an element in a linked list. */
@@ -22,6 +55,32 @@ int lstset(List *lst, int idx, void *val) {
 
 /* lstadd: Adds an element to a linked list. */
 int lstadd(List *lst, int idx, void *val) {
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->val = val;
+    node->next = NULL;
+
+    if (idx == 0) {
+        node->next = lst->head;
+        lst->head = node;
+    }
+    else {
+        Node *tmp = lst->head;
+        int i;
+
+        for (i = 0; i < idx - 1; i++) {
+            tmp = tmp->next;
+        }
+
+        node->next = tmp->next;
+        tmp->next = node;
+    }
+
+    lst->size++;
+
+    /* NOTE: Since we emulated pass-by-reference behavior by taking as argument
+     *       a pointer to a list, any modifications to the list are already
+     *       visible outside; this return value is an error code to indicate,
+     *       for example, whether or not the index was in-bounds. */
     return 0;
 }
 
--- 04.21-structures/list.h
+++ 04.21-structures/list.h
@@ -1,10 +1,22 @@
-/* Node: Represents an element in a linked list. */
-void *val;
-void *next;
+/* Node: Represents an element in a linked list.
+ * NOTE: This defines a new type named "struct Node" which is eventually
+ *       renamed to "Node", but that renaming will not take effect until after
+ *       the structure definition, so we must still use "struct Node" inside
+ *       the definition. */
+typedef struct Node {
+    void *val;
+    struct Node *next;
+} Node;
 
-/* List: Represents a linked list. */
-void *head;
-int size;
+/* List: Represents a linked list.
+ * NOTE: By grouping these two members into a structure, we can treat them as
+ *       a single unit, a new type named "struct List" (which is then renamed
+ *       to "List"). Since "struct Node" has already been renamed to "Node", we
+ *       can use "Node" inside this definition. */
+typedef struct List {
+    Node *head;
+    int size;
+} List;
 
 List *lstcreate(void);
 void lstdestroy(List *);
