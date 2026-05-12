71a68b0e6921606b14d929017e1a73bf22d1273a
Christopher E. Siu <cesiu@calpoly.edu>
2026-05-12 07:56:20 -0700

    Added base code for fork.

---
+++ 05.12-processes/search.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define LINELEN 81
+
+int fsearch(char *, char *);
+
+int main(int argc, char *argv[]) {
+    int status = EXIT_FAILURE;
+
+    return status;
+}
+
+/* fsearch: Prints occurrences of a string in a file. */
+int fsearch(char *fname, char *str) {
+    int status = EXIT_FAILURE;
+    char buf[LINELEN];
+    FILE *file = fopen(fname, "r");
+
+    while (fgets(buf, LINELEN, file) != NULL) {
+        if (strstr(buf, str) != NULL) {
+            printf("%s: %s", fname, buf);
+            status = EXIT_SUCCESS;
+        }
+    }
+
+    fclose(file);
+    return status;
+}
