bf8b498336f3f93d0a1fa898bbb8119c55eff3d2
cesiu <cesiu@calpoly.edu>
2026-05-13 08:19:46 -0700

    Added example of fork and wait.

--- 05.12-processes/search.c
+++ 05.12-processes/search.c
@@ -1,3 +1,6 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -7,9 +10,39 @@
 int fsearch(char *, char *);
 
 int main(int argc, char *argv[]) {
-    int status = EXIT_FAILURE;
+    int status = EXIT_FAILURE, i;
+    pid_t child;
 
-    return status;
+    /* NOTE: "fork" duplicates the parent process -- the child will have the
+     *       same contents in memory, which means the child will be executing
+     *       the same instructions. Effectively, fork is called once in the
+     *       parent and it returns *twice* to both parent and child. */
+    for (i = 2; i < argc; i++) {
+        if ((child = fork()) == 0) {
+            printf("%ld is the child of %ld.\n", (long)getpid(), (long)getppid());
+            return fsearch(argv[i], argv[1]);
+        }
+        else {
+            printf("%ld is the parent of %ld.\n", (long)getpid(), (long)child);
+        }
+    }
+
+    /* NOTE: Parents are responsible for waiting for their children, so as to
+     *       clean up any resources associated with the resulting zombies. In
+     *       this case, however, we need to make sure that all of the children
+     *       are created before we start waiting. */
+    for (i = 2; i < argc; i++) {
+        child = wait(&status);
+
+        if (WIFEXITED(status)) {
+            printf("%ld exited with status %d.\n", (long)child, WEXITSTATUS(status));
+        }
+        else {
+            printf("%ld exited abnormally.\n", (long)child);
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
 
 /* fsearch: Prints occurrences of a string in a file. */
