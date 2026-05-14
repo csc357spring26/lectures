8a94ea88a42dfde6318b9b09ee7831e4a2085317
cesiu <cesiu@calpoly.edu>
2026-05-14 14:26:21 -0700

    Added example of exec and signals.

--- 05.14-signals/parent.c
+++ 05.14-signals/parent.c
@@ -9,10 +9,30 @@ int main(void) {
     pid_t child;
 
     if ((child = fork()) == 0) {
-        printf("%ld is a child of %ld.\n", (long)getpid(), (long)getppid());
+        /* NOTE: By replacing the child process with the contents of another
+         *       executable, the child is no longer limited to executing only
+         *       the code that it inherited from its parent. By convention, the
+         *       first argument should always be the name of the executable. */
+        execlp("./child", "child", NULL);
+
+        /* NOTE: Assuming "exec" succeeds, the current process is replaced with
+         *       the contents of another executable, which means any code below
+         *       "exec" ceases to exist. Effectively, "exec" is called once
+         *       and, if it succeeds, it *never returns at all*. */
+        perror("execlp");
+        return EXIT_FAILURE;
     }
     else {
         printf("%ld is the parent of %ld.\n", (long)getpid(), (long)child);
+
+        sleep(1);
+        kill(child, SIGINT);
+        sleep(1);
+        kill(child, SIGQUIT);
+        sleep(1);
+        kill(child, SIGSTOP);
+        kill(child, SIGKILL);
+
         child = wait(&status);
 
         if (WIFEXITED(status)) {
