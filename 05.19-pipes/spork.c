f6ef6597ade885e5faf247482847d17a4a2951be
cesiu <cesiu@calpoly.edu>
2026-05-19 13:30:43 -0700

    Added examples of timers, resource limits, and pipes.

--- 05.19-pipes/spork.c
+++ 05.19-pipes/spork.c
@@ -1,6 +1,69 @@
 #include <sys/types.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+
+/* NOTE: For simplicity, we'll assume we only spork one child at a time, else
+ *       this would need to be some sort of dynamically allocated collection of
+ *       multiple child PIDs. */
+static pid_t child;
+
+/* NOTE: Since a handler effectively has no caller, it cannot take any more
+ *       arguments or return a value. Instead, to communicate data to and from
+ *       a handler, we can leave it in global variables. If this makes us
+ *       uncomfortable, we can declare those variables as "static", so that
+ *       they are not accessible outside of this file. */
+void handler(int signum) {
+    (void)signum;
+
+    kill(child, SIGKILL);
+}
 
 /* spork: Creates a new resource-limited process. */
 pid_t spork(time_t timeout) {
-    return 0;
+    struct rlimit limit;
+    struct sigaction action;
+    struct itimerval timer;
+
+    if ((child = fork()) == 0) {
+        /* NOTE: Set the child's soft and hard limits on number of processes to
+         *       1, so that the child cannot create any children of its own. */
+        getrlimit(RLIMIT_NPROC, &limit);
+        limit.rlim_cur = 1;
+        limit.rlim_max = 1;
+        setrlimit(RLIMIT_NPROC, &limit);
+    }
+    else {
+        /* NOTE: Install a handler to respond to SIGARLM -- note that this must
+         *       be done *before* setting the corresponding timer, else there
+         *       is a chance the timer could elapse before the handler is
+         *       installed. */
+        action.sa_handler = handler;
+        action.sa_flags = SA_RESTART;
+        sigemptyset(&action.sa_mask);
+        sigaction(SIGALRM, &action, NULL);
+
+        /* NOTE: Create a timer to send a SIGARLM after "timeout" seconds have
+         *       elapsed in real time. */
+        timer.it_value.tv_sec = timeout;
+        timer.it_value.tv_usec = 0;
+
+        /* NOTE: In this case, the timer does not need to repeat; if it did,
+         *       the "it_interval" determines the value that will be used to
+         *       reset the "it_value" every time the timer elapses. */
+        timer.it_interval.tv_sec = 0;
+        timer.it_interval.tv_usec = 0;
+        setitimer(ITIMER_REAL, &timer, NULL);
+
+        /* NOTE: Compared to simply sleeping, setting a timer allows us to do
+         *       something else while waiting for the timer to elapse. If we
+         *       don't have anything else useful to do, "pause" suspends
+         *       execution until a signal is received, so as to avoid the
+         *       need for "busy waiting".
+         * pause(); */
+    }
+
+    return child;
 }
--- 05.19-pipes/test.c
+++ 05.19-pipes/test.c
@@ -6,27 +6,53 @@
 #include "spork.h"
 
 int main(void) {
-    int status;
+    int status, fds[2];
     pid_t child;
 
+    /* NOTE: Create a pipe to send data from the parent to the child. By doing
+     *       this before creating the child process, the child will inherit
+     *       access to the pipe by virtue of being a duplicate of the parent. */
+    pipe(fds);
+
     if ((child = spork(5)) == 0) {
+        int n;
+        char buf[17];
+
         printf("%ld is the child of %ld.\n", (long)getpid(), (long)getppid());
 
-        while (fork() < 0) {
-            perror("fork");
-            sleep(1);
-        }
-    }
-    else {
-        printf("%ld is the parent of %ld.\n", (long)getpid(), (long)child);
-        child = wait(&status);
+        close(fds[1]);
 
+        while ((n = read(fds[0], buf, 16)) > 0) {
+            buf[n] = '\0';
+            printf("%ld read \"%s\" from the pipe.\n", (long)getpid(), buf);
+        }
+
+        close(fds[0]);
+    }
+    else if (child > 0) {
+        printf("%ld is the parent of %ld.\n", (long)getpid(), (long)child);
+
+        close(fds[0]);
+
+        write(fds[1], "YOU HAVE FIVE SECONDS TO GO TO YOUR ROOM!", 41);
+
+        /* NOTE: Reading from and writing to pipes are blocking calls; they
+         *       will wait for data to be available. By closing the parent's
+         *       write end, we notify the child that there will be no more
+         *       data coming in the future. */
+        close(fds[1]);
+
+        child = wait(&status);
         if (WIFEXITED(status)) {
             printf("%ld exited status %d.\n", (long)child, WEXITSTATUS(status));
         }
         else {
             printf("%ld exited abnormally.\n", (long)child);
         }
+    }
+    else {
+        perror("spork");
+        return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
